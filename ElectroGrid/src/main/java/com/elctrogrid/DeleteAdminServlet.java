a1da6d0ee692c1e47b7580d825539f2d613084f6
admin <it20246914@my.sliit.lk>
2022-04-22 12:01:59 +0530

    Innitial Commit

---
+++ ElectroGrid/src/main/java/com/elctrogrid/DeleteAdminServlet.java
@@ -0,0 +1,42 @@
+package com.elctrogrid;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/DeleteAdminServlet")
+public class DeleteAdminServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String id = request.getParameter("cusid");
+		boolean isTrue;
+		
+		isTrue = AdminDBUtil.deleteAdmin(id);
+		
+		if (isTrue == true) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("admininsert.jsp");
+			dispatcher.forward(request, response);
+		}
+		else {
+			
+			List<Admin> cusDetails = AdminDBUtil.getAdminDetails(id);
+			request.setAttribute("cusDetails", cusDetails);
+			
+			RequestDispatcher dispatcher = request.getRequestDispatcher("adminaccount.jsp");
+			dispatcher.forward(request, response);
+		}
+		
+		
+		
+	}
+
+}
